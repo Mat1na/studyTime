22d96385ca93a86e195a1a4877ea9b0f55220ba1
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-06-19 13:44:47 +0200

    update

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -7,9 +7,27 @@
 // }
 
 
+// document.querySelector('button'),addEventListener("click",()=>alert("i got clicked")
+// )
+
+
+// numberOfDrumButtons=document.querySelectorAll(".drum").length
+
 for(i=0; i<document.querySelectorAll(".drum").length; i++){
 
-document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-    alert("I got clicked")
-})
+  document.querySelectorAll(".drum")[i].addEventListener("click", ()=>{switch (key) {
+    case value:
+      
+      break;
+  
+    default:
+      break;
+  }})
+
+
+  
 }
+
+
+// {var audio = new Audio('sounds/tom-1.mp3');
+//   audio.play();}
